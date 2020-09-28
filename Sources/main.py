14142df80177610e3c6ae4ad5f1b0d8b113da568
PierreSchauli <pierrelixenbuhl@gmail.com>
2020-09-28 11:54:12 +0200

    Corrigé

--- Sources/main.py
+++ Sources/main.py
@@ -19,7 +19,7 @@ driver = webdriver.Chrome(resource_path('./chromedriver.exe'))
 driver.get(r"https://monbureaunumerique.fr") # on va sur MBN
 driver.find_element_by_xpath(r"/html/body/div/div/div/div[2]/a[2]").click() # on clique sur connexion
 driver.find_element_by_xpath(r"/html/body/main/div/div/div[1]/div/div/form/fieldset[1]/legend/button").click() # on clique sur élève
-driver.find_element_by_xpath(r"/html/body/main/div/div/div[1]/div/div/form/fieldset[1]/ul/li[4]/div/label").click() # on clique sur Strasbourg
+driver.find_element_by_xpath(r"/html/body/main/div/div/div[1]/div/div/form/fieldset[1]/ul/li[3]/div/label").click() # on clique sur Strasbourg
 driver.find_element_by_xpath(r'//*[@id="button-submit"]').click() # on valide
 driver.find_element_by_xpath(r'//*[@id="user"]').send_keys(name) # on met l'identifiant
 driver.find_element_by_xpath(r'//*[@id="password"]').send_keys(mdp) # on met le mdb
